beaa0e962480b1851fa270d948b1cea79e93ae72
jqb <kuba.janoszek@gmail.com>
2015-02-22 22:58:20 +0100

    penv:cli: new (fixed) install scripts

--- penv/cli.py
+++ penv/cli.py
@@ -6,20 +6,49 @@ from .base import Penv
 
 
 abspath = os.path.abspath
-override_cd_bash_function = """
+install_script_bash = """
 function cd () {
-    builtin cd "$@" && eval "$(penv scan)"
+    LOCK_FILE=/tmp/.penv-lock
+
+    if [ -f $LOCK_FILE ]
+    then
+        builtin cd "$@"
+    else
+        touch $LOCK_FILE
+        builtin cd "$@" && eval "$(penv scan)"
+        rm $LOCK_FILE
+    fi
+}
+
+function penv-ify () {
+    VIRTUAL_ENV_DIRECTORY_PATH=$1
+
+    mkdir -p .penv/.plugins
+
+    if [ -d "$VIRTUAL_ENV_DIRECTORY_PATH" ]; then
+        VIRTUAL_ENV_DIRECTORY_NAME="`basename $VIRTUAL_ENV_DIRECTORY_PATH`"
+        echo "$VIRTUAL_ENV_DIRECTORY_NAME" > .penv/default
+        builtin cd .penv                                                     && \
+            ln -s ../$VIRTUAL_ENV_DIRECTORY_PATH $VIRTUAL_ENV_DIRECTORY_NAME && \
+            builtin cd ..
+    else
+        echo "venv" > .penv/default
+        virtualenv .penv/venv --prompt="(`basename \`pwd\``)"
+    fi
+
+    cd .
 }
 """
 
 
 @click.group(invoke_without_command=True)
-@click.option('--override-cd-bash', default=False, is_flag=True,
-              help='Just prints the command to be evaluated by bash.')
+@click.option('--install-script', default='bash',
+              help=('Just prints the command to be evaluated by given shell '
+                    '(so far only bash supported)'))
 @click.pass_context
-def cli(ctx, override_cd_bash):
-    if ctx.invoked_subcommand is None and override_cd_bash:
-        return click.echo(override_cd_bash_function)
+def cli(ctx, install_script):
+    if ctx.invoked_subcommand is None and install_script:
+        return click.echo(install_script_bash)
 
     if ctx.invoked_subcommand is None:
         return click.echo(ctx.command.get_help(ctx))
@@ -27,13 +56,13 @@ def cli(ctx, override_cd_bash):
     # Maybe I'll make "place" customizable at some point
     ctx.obj = {
         'place': abspath('.'),
-        'override_cd_bash': override_cd_bash,
+        'install_script': install_script,
     }
 
 
-# $> penv [--place=/path/to/place/] scan
+# $> penv scan
 @cli.command('scan')
 @click.pass_context
-def cli_stan(ctx, env=Penv()):
+def cli_scan(ctx, env=Penv()):
     place = ctx.obj['place']
     return click.echo(env.lookup(place))
