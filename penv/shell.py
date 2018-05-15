a8690f914cc5b3843164bb3988cd3005b910ceb1
jqb <kuba.janoszek@gmail.com>
2018-05-15 14:49:57 +0200

    shell: typo / missing space

--- penv/shell.py
+++ penv/shell.py
@@ -8,7 +8,7 @@ import errno
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python > 2.5
+    except OSError as exc:  # Python > 2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
